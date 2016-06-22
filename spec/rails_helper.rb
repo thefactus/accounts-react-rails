c2487455c01e83dacbc8611a881526f0853c0c36
Pablo Bello <thefactus@gmail.com>
2016-06-22 15:02:33 -0300

    Setting up Rspec

---
+++ spec/rails_helper.rb
@@ -0,0 +1,23 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+require 'rspec/rails'
+require 'capybara/rspec'
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.include Request::JsonHelpers, :type => :controller
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
+end
